83f8c3444bc5902d430d5b650fd3a6fac8a586f4
Mazmanyan Yura <mazmanya@mail.ru>
2024-03-23 01:04:36 +0300

    Работа на отображением первой страници MAINVIEW. Создан обстрактный класс ABSTRACTVIEW от него расшираются все страници

--- src/app.js
+++ src/app.js
@@ -1,4 +1,24 @@
 import "../static/reset.css";
 import "../static/global.css";
+import "../src/app.css";
+import { MainView } from "./views/main/main.js";
+class App {
+  routes = [
+    {
+      path: "",
+      view: MainView,
+    },
+  ];
+  constructor() {
+    window.addEventListener("hashchange", this.route.bind(this));
+  }
+  route() {
+    if (this.currentView) this.currentView.destroy();
 
-import "./app.css";
+    const view = this.routes.find((r) => r.path === location.hash).view;
+    this.currentView = new view();
+    this.currentView.render();
+    this.currentView.render();
+  }
+}
+new App().route();
