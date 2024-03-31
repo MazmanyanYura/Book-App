604764491bfdc295d14e0ef54136a0bea45d6fd1
Mazmanyan Yura <mazmanya@mail.ru>
2024-03-31 20:13:38 +0300

    Верстка HEADER

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import { DivComponent } from "../../common/div-component";
+import "./Header.css";
 
 export class Header extends DivComponent {
   constructor(appState) {
@@ -10,8 +11,30 @@ export class Header extends DivComponent {
     this.element.classList.add("header");
     this.element.innerHTML = ` 
     <div>
-        <img src="/static/logo.svg" alt="Логотип" />
-    </div>`;
+      <img src="/static/images/logo.svg" alt="Логотип" />
+    </div>
+    <ul class="menu">
+      <li class="menu__item">
+        <a class="menu__item-link" href="#">
+          <img src="/static/images/search-icon.svg" alt="Поиск иконка" />
+        Поиск книг
+        </a>
+      </li>
+      <li class="menu__item">
+        <a class="menu__item-link" href="#">
+          <img 
+          src="/static/images/favorites-icon.svg" 
+          alt="Избранное иконка" />
+          Избранное
+        </a>
+      </li>
+      <li class="menu__item">
+          <p class="menu__counter">
+            ${this.appState.favorites.length}
+          </p>
+      </li>
+    </ul>
+    `;
     return this.element;
   }
 }
